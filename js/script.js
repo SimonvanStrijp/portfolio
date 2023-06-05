8059d27223a1d972706f62ba0ffc32ac3232f4b3
SimonvanStrijp <36440541+SimonvanStrijp@users.noreply.github.com>
2023-06-05 14:30:33 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var projectActive = false;
+
 //loop through all the divs with class prj
 console.log("hoi");
 
@@ -10,7 +12,9 @@ console.log("hoi");
 
     $('.prj-container-header').each(function(index) {
         console.log("this is: " + this);
-        var targetHeader = "projects/project" + (index+1) + ".html header" ;
+       
+        // var targetHeader = "projects/project" + (index+1) + ".html header" ;
+        var targetHeader = "projects/" +  $(this).parent().attr("id") + ".html header";
         $(this).load(targetHeader);
         index = 0; 
     });
@@ -18,11 +22,22 @@ console.log("hoi");
     $('.hiddenButton').hide(0);
 
     
+    // load CV
+    $('#cv-container').load('cv.html');
+
+    
     
     $('.prj-button').click(function() {
         var target = "projects/" + $(this).parent().attr("id") + ".html #body";
         console.log("target: " + target);
         if ($(this).parent().hasClass('prj-active')) {
+            projectActive = false;
+            $('.prj-dummy').show(300);
+            console.log("projectActive: " + projectActive);
+            toggleCV();
+            $('html, body').animate({
+                scrollTop: $("#projects").offset().top
+            }, 250);
             $(".prj").each(function() {
                 $(this).removeClass('prj-active');
                 $(this).removeClass('prj-inactive'); });
@@ -30,8 +45,16 @@ console.log("hoi");
                 $(this).show(300); });
             $('.prj-container-body').empty();
             $('.prj-header').each().addClass('.prj-header-inactive');
+            
 
         } else {
+            projectActive = true;
+            console.log("projectActive: " + projectActive);
+            toggleCV();
+            $('.prj-dummy').hide(300);
+            $('html, body').animate({
+                scrollTop: $("#projects").offset().top
+            }, 250);
             $(".prj").each(function() {
                 $(this).remove('prj-active');
                 $(this).addClass('prj-inactive'); });
@@ -41,11 +64,11 @@ console.log("hoi");
             $(this).parent().addClass('prj-active');
             $('.prj-active > .prj-container-body').load(target, function() {
                 $(this).show(300);
-                // $('html, body').animate({scrollTop: $('#projects')}, 2000);
             });
             $('.prj-header').each().removeClass('.prj-header-inactive');
             
         }
+        
         });
 
     
@@ -59,19 +82,40 @@ console.log("hoi");
 
             if ($('#about-container').hasClass('about-active')) {
                 $('#about-container').hide(0);
-                $('#about-container').load(target);
-                $('#about-container').show(300);
-                $('.hiddenButton').show(300);
+                $('#about-container').load(target, function(){
+                    $('#about-container').show(300);
+                    $('.hiddenButton').show(300);
+                    $('html, body').animate({
+                        scrollTop: $("#about-container").offset().top
+                    }, 250);
+                });
+                
             } else {
-                $('#about-container').empty();
-                $('#about-container').hide(300);
                 $('.hiddenButton').hide(300);
+                $('#about-container').hide(300,function(){
+                    $('#about-container').empty();
+                    
+                
+            });
+                
             }
         }
     );
 
 
+    // open/close CV
+    function toggleCV(){
+        if (projectActive) {
+            // alert("project active");
+            $('#cv-container').hide(300);
+        } else {
+            $('#cv-container').show(300);
+        }
+    }
+
+
+
+
     
     
-    // load CV
-    $('#cv-container').load('cv.html');
+    
